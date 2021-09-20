2131e678a2d861e267273040f1e68136fd06f37d
pinkpig777 <63622191+pinkpig777@users.noreply.github.com>
2021-09-20 14:29:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Created on Sat Jan 23 11:30:26 2021
 """
 from automatic_novel_getter import *
 main()
-from line_noify import lineNotifyMessage
+from line_notify import lineNotifyMessage
 text = []
 with open(r'C:\Users\charl\OneDrive\桌面\python\web_crawler\book.txt','r',encoding = 'utf-8') as fh :
     url = fh.readline()
